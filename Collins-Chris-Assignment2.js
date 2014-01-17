f0970bed5c2df863502b997971a74b59e402e974
Chris Collins <chrishcollins@fullsail.edu>
2014-01-16 23:33:14 -0500

    Collins-Chris-Project2
    SDI 1401 JavaScript

--- Collins-Chris-Assignment2.js
+++
@@ -1,8 +0,0 @@
-alert("JavaScript works!");
-
-//Chris Collins
-//SDI 1401
-//Project 2
-//TTILE OF PROJECT
-
-//variables
---
+++ Collins-Chris-Project2.js
@@ -0,0 +1,82 @@
+alert("Code for Project 2!");
+
+
+//Chris Collins
+//SDI 1401
+//January 16, 2014
+//Code for Project 2
+
+
+
+//variables
+var myChoice;
+var time = 38; //iterator
+var collegeHours;
+var mobileSalary = ("$92,000");
+var engineerSalary = ("$80,000");
+var firstCollege = ("Old Dominion University");
+var currentCollege = ("Full Sail University");
+
+
+//procedure
+
+
+myChoice = prompt("Should I go back to college?", "Enter Yes or No");  //prompting user for input "Yes or No."
+
+if (myChoice === "Yes") {  //argument
+	
+	console.log(myChoice +  " I Should go back and pursue my education!");
+} else {
+	console.log("I still think I should go back to college, even if you don't want me to.");
+}
+
+
+
+//confirm
+
+myFuture = confirm("Will I have enough time for College?  Click OK for True or Cancel for False"); //confirm with Boolean
+
+if (myFuture ===true) {
+	
+	console.log("I need to decide which College is best for me.");
+} else {
+	console.log("Maybe I can take a few classes at a time.");
+}	
+
+if (myFuture ===true) {
+	console.log("I guess I need to see if I have enough time for College.");
+	} else {
+	console.log("I am too busy right now for College");
+};
+
+
+//While Loop
+
+	while (time < 38) {
+    if (timeWork == 0); {
+    	time --;
+	    
+    }
+    collegeHours = ("I only have" + time + " hours, so I won't have enough time to get my work done.");
+    return time;
+}
+break;    
+
+
+//strings
+var choiceOne = ("I previously attended Old Dominion University for Mechanical Engineering");
+var choiceTwo = ("For several years I have wanted to attend Full Sail University");
+var collegePick = function(college) {
+	decision("choiceTwo" + "," + " so I will apply to attend there.")
+	return decision;
+};
+
+var finalChoice = collegePick("I will finally make a choice.");	
+
+//numbers
+
+
+
+console.log("I didn't like the coursework at " + firstCollege.);
+console.log(currentCollege + "has been where I wanted to go all along.");
+
